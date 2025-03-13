a59fe5fdeaead3b4a4479a1e6c3cb40f462c6a7b
Ocean_Dev2 <zakaryabaouali255@gmail.com>
2025-03-13 12:25:59 +0000

    ds

--- src/App.jsx
+++ src/App.jsx
@@ -7,20 +7,18 @@ import View from './pages/View'
 import Footer from './components/Footer'
 import Book from './pages/Book'
 import { Routes, Route } from 'react-router-dom'
-import RouterComponent from './router'
+
 
 function App() {
   return (
     <>
       <NavBar />
-  
        <Routes> 
         <Route path='/' element={<Home/>}/>
         <Route path='/destinations' element={<Destinations/>}/>
         <Route path='/travel' element={<Travel/>}/>
         <Route path='/view' element={<View/>}/>
         <Route path='/book' element={<Book/>}/>
-        
       </Routes>
      <Footer/>
       
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,11 @@
 import React from "react"
-import vide from "../assets/vide.mp4"
+
 import { AiOutlineSearch } from "react-icons/ai";
 import {BiSearch} from "react-icons/bi"
 const Hero = () =>{
     return(
         <div className="w-full h-screen relative">
-<video className="w-full h-full object-cover" src={vide} autoPlay loop muted/>
+<video className="w-full h-full object-cover" src={"https://res.cloudinary.com/doi4cmnue/video/upload/v1741714828/bfwygznsy7xmfc7hruci.mp4"} autoPlay loop muted/>
 <div className="absolute w-full h-full top-0 left-0 border-gray-900/30">
         <div className="absolute top-0 w-full h-full flex flex-col justify-center text-center text-white p-4  ">
             <h1 className="text-color p-4">Discover Agadir</h1>
