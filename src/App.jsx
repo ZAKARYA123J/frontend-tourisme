5c31ee2a478cb406691741af305460708babe0f9
Maroua <marouaig05@gmail.com>
2025-03-18 10:11:05 +0000

    Nj

--- src/App.jsx
+++ src/App.jsx
@@ -9,31 +9,34 @@ import Login from './Auth/Login';
 import Register from './Auth/Register';
 import ForgotPassword from './Auth/ForgotPassword';
 import ResetPassword from './Auth/ResetPassword';
-import Festivals from '../src/pages/Festivals';
+import Festivals from './pages/Festivals';
 import { Routes, Route } from 'react-router-dom'
-import { Link } from 'react-router-dom'
 
 
 
 function App() {
   return (
-    <>
-      <NavBar />
-      <main className="flex-grow"> 
+    
+     <> 
+      <div className="flex flex-col min-h-screen">
+       <NavBar />
+       <main className="flex-grow">
        <Routes> 
         <Route path='/' element={<Home/>}/>
         <Route path='/destinations' element={<Destinations/>}/>
-        <Route path="/festivals" element={<Festivals />} />        <Route path='/view' element={<View/>}/>
+        <Route path="/festivals" element={<Festivals />}/>    
+            <Route path='/view/:id' element={<View/>}/>
         <Route path='/book' element={<Book/>}/>
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
      <Route path="/forgot-password" element={<ForgotPassword />} />
     <Route path="/reset-password" element={<ResetPassword />} />
       </Routes>
-
       </main>
-    <Footer/>
-    </>
+    
+      <Footer/>
+    </div>
+ </>
   )
 }
 
